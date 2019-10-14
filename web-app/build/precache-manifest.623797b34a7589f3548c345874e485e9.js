4d4d91d3782176a325dee8cff1ecd543d3a3d5e6
GrégoireMorane <gh.morane@gmail.com>
2019-10-14 15:43:00 +0200

    feat(*): quest done

---
+++ web-app/build/precache-manifest.623797b34a7589f3548c345874e485e9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118b07e2676c05fc7186a155acf07722",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39294fa9cd6ef947e5ad",
+    "url": "/static/js/2.f0b0e0d5.chunk.js"
+  },
+  {
+    "revision": "1755ca72ed761527c805",
+    "url": "/static/js/main.da39f928.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
